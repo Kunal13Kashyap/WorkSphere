79bfbdeb3d232f84e9f94fed3d94bd7ae08d9eaf
Kunal13Kashyap <kunalkashyap2015.kk@gmail.com>
2026-05-17 00:13:35 +0530

    refactor: Remove unnecessary code

--- backend/src/middlewares/error.middleware.js
+++ backend/src/middlewares/error.middleware.js
@@ -1,8 +1,4 @@
 export const errorHandler = (err,req,res,next)=>{
-
-    // just for testing, have to remove it later
-    console.error(err);
-    /* ---------------------------------------- */
     
     const errMessage = err.isOperational ? err.message : "Internal Server Error";
     const statusCode = err.statusCode || 500;
