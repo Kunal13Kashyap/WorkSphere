7cc1d232f70b7cbd6aa53cff698639ba51b51439
Kunal13Kashyap <kunalkashyap2015.kk@gmail.com>
2026-07-04 18:54:23 +0530

    feat: Update org route

--- backend/src/modules/organization/organization.routes.js
+++ backend/src/modules/organization/organization.routes.js
@@ -3,10 +3,12 @@ import { inviteController, roleUpdateController } from "./organization.controlle
 import { auth } from "../../middlewares/auth.middleware.js";
 import { authorizeRoles } from "../../middlewares/rbac.middleware.js";
 import { ROLES } from "../../utils/roles.js";
+import { inviteSchema } from "../../validations/index.js";
+import { validate } from "../../middlewares/validate.middleware.js";
 
 const router = express.Router();
 
-router.post("/invite",auth,authorizeRoles(ROLES.ADMIN),inviteController);
+router.post("/invite",auth,authorizeRoles(ROLES.ADMIN),validate(inviteSchema),inviteController);
 
 router.patch("/users/:userId",auth,authorizeRoles(ROLES.ADMIN),roleUpdateController);
 
